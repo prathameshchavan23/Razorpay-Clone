b0a7290116cf9571b6214f89c3502aa627f366d8
Prathamesh <chavanprathamesh2303@gmail.com>
2023-10-03 19:24:19 +0530

    initial_commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {fontFamily: {
+      mullish: ["mulish" , "sans-serif"],
+    }, 
+      colors:{
+        deepBlue: "#02042a",
+        LightBlue: "#2b84ea",
+        LightBlue300: "#4b94ed",
+        LightBlue500: "#0b72e7",
+        greenLight: "#61cea6",
+        grayText: "#818597",
+        lightGray: "#e2e2e2",
+        grayBlue: "#344a6c",
+        grayBlueHead: "#162f56",
+        gray2: "#525a76",
+        },},
+      },
+  plugins: [],
+}
